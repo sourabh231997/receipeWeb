19f815a0cd482690d4f752714b5f7d42529391e6
sourabh231997 <sourabh.ghildiyal01@gmail.com>
2021-10-21 00:11:36 +0530

    add toaster

--- receipeWeb/src/app/contact/contact.component.ts
+++ receipeWeb/src/app/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { UserService } from '../_services/user.service';
+import { NotificationService } from '../_services/notification.service'
 import * as $ from 'jquery';
 
 @Component({
@@ -12,7 +13,7 @@ export class ContactComponent implements OnInit {
   model: any = {};
   complexForm:any;
 
-  constructor(fb: FormBuilder, private userService: UserService) {
+  constructor(fb: FormBuilder, private userService: UserService,private notifyService : NotificationService) {
     this.complexForm = fb.group({
     })
   }
@@ -32,7 +33,13 @@ export class ContactComponent implements OnInit {
       'message':this.model.message
     }
     this.userService.sendUserMailData(userData).subscribe(success => {
-      console.log(success);
+      if(success && success['err']){
+        if(this.model.user_name == '') this.notifyService.showError("name", "Field Required")
+        if(this.model.email == '') this.notifyService.showError("email", "Field Required")
+        if(this.model.phone == '') this.notifyService.showError("phone", "Field Required")
+      }else{
+        this.notifyService.showSuccess("Mail sent successfully !!", "Rasayati")
+      }
     })
   }
 
